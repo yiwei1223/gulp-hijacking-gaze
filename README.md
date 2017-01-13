7d23e2d7f9e7e6737923e2ba1908941e8560d22f
yiwei <yiwei@xiaomi.com>
2017-01-13 15:49:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gulp-hijacking-gaze
+一个自动在html文件注入注释代码的插件
+
+# Install
+Install with npm
+
+	npm install --save-dev gulp-hijacking-gaze
+
+#API
+
+	hijackingGaze('<!-->xxxx<-->')
---
+++ index.js
@@ -0,0 +1,34 @@
+/**!
+ * gulp-tinify-img 1.0.0 (c) 2017 Yi wei - MIT license
+ * @desc 动态的在html尾部加入诱导劫持注释代码
+ */
+'use strict';
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+/**
+ * @desc 插件主体
+ * @param gaze 注释代码
+ */
+module.exports = function (gaze) {
+    var _gaze = gaze || '<!-- <script></script> -->' +
+        '<!-- <body><script src="js/$touch.js"></script></body></html> --></body><!-- <body><script src="js/$touch.js"></script></body></html> --></html>' +
+        '<!-- <html><script src="js/$touch.js"></script></html> -->';
+
+    // 压缩处理
+    return through.obj(function (file, enc, cb) {
+        var that = this;
+        if (file.isStream()) {
+            var _sourceHtml = file.contents.toString('utf8').replace(/\s*\<\/body\>\s*\<\/html\>\s*/gi, _gaze);
+            file.contents = new Buffer(_sourceHtml);
+            cb(null, file);
+        }
+
+        if (file.isBuffer()) {
+            var sourceHtml = file.contents.toString('utf8').replace(/\s*\<\/body\>\s*\<\/html\>\s*/gi, _gaze);
+            file.contents = new Buffer(sourceHtml);
+            cb(null, file);
+        }
+    });
+};
